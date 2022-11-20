03ba5ad033b708b2d6cfcecef24f738a3a568b8b
Alqayadhi <mo.alqayadhi@gmail.com>
2022-11-20 18:12:00 +0100

    final code of the app

--- 5-SecondDictionaryApp/app.js
+++ 5-SecondDictionaryApp/app.js
@@ -13,24 +13,29 @@ btn.addEventListener("click", () =>{
             result.innerHTML = `
             <div class="word">
                 <h3>${inpWord}</h3>
-                <button id="btn">
+                <button onclick="playSound()">
                     <i class="fas fa-volume-up"></i>
                 </button>
             </div>
             <div class="details">
                 <p>${data[0].meanings[0].partOfSpeech}</p>
-                <p>/${data[0].phonetic}/</p>
+                <p>${data[0].phonetic}</p>
             </div>
             <p class="word-meaning">
-                Lorem ipsum, dolor sit amet 
-                consectetur adipisicing elit. 
-                Officiis obcaecati vero fugiat.
+                ${data[0].meanings[0].definitions[0].definition}
             </p>
             <p class="word-example">
-                Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. 
-                Unde, totam!
+                ${data[0].meanings[0].definitions[0].example};
             </p>
             `;
-        });
-})
+
+        sound.setAttribute('src', `${data[0].phonetics[1].audio}`);
+        
+        }).catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`
+        })
+});
+
+function playSound() {
+    sound.play();
+}
