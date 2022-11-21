d1b2f804351abcf5ab4ef4c5d5c9c21d770d8ed2
Alqayadhi <mo.alqayadhi@gmail.com>
2022-11-21 23:58:49 +0100

    finishing the forntend code

--- 6-ThirdDictionaryApp/app.js
+++ 6-ThirdDictionaryApp/app.js
@@ -1,1 +1,36 @@
-console.log('hello');
+const dictionary = 
+[
+    {
+        word:"apple",
+        def:"A round fruit with seeds at its center",
+        rel:["mango","pear","guava"]
+    },
+    {
+        word:"baby",
+        def:"the young one of a human",
+        rel:["child","toddler","teen"]
+    },
+    {
+        word:"car",
+        def:"transports people from place to place",
+        rel:["mango","pear","guava"]
+    },
+    {
+        word:"computer",
+        def:"An electronic device",
+        rel:["laptop","tablet","palmtop"]
+    },
+    {
+        word:"mosquito",
+        def:"An insect",
+        rel:["ant","beetle","butterfly"]
+    }
+];
+
+// fill the dictionary with words
+init = function(){
+    for (let i = 0; i < dictionary.length; i++) {
+       document.getElementById("word_list").innerHTML += "<li onclick = 'show(' + i + ')'>" +
+       dictionary[i].word + '</li>';
+    }
+}
