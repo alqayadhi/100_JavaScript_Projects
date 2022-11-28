11c40abf266a98271bae10bdf4713867162dc627
Alqayadhi <mo.alqayadhi@gmail.com>
2022-11-28 20:28:34 +0100

    final html css

--- 8-FifthDictionaryApp/app.js
+++ 8-FifthDictionaryApp/app.js
@@ -1,1 +1,11 @@
-console.log('hi');
+// input field
+let text = document.querySelector('#text');
+// search button 
+let search = document.querySelector('#search');
+// content section => main container
+let data = document.querySelector('#data');
+
+let loadData = () =>{
+    // api for dictionary 
+    let api = ``;
+}
