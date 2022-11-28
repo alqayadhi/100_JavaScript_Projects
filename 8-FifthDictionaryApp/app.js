ea5ab8212642e36c7300647ef52d0de6b6854f0b
Alqayadhi <mo.alqayadhi@gmail.com>
2022-11-28 22:01:29 +0100

    final results

--- 8-FifthDictionaryApp/app.js
+++ 8-FifthDictionaryApp/app.js
@@ -7,5 +7,50 @@ let data = document.querySelector('#data');
 
 let loadData = () =>{
     // api for dictionary 
-    let api = ``;
+    const api = `https://api.dictionaryapi.dev/api/v2/entries/en/${text.value}`;
+   
+    fetch(api)
+    .then(response => response.json())
+    .then(result => {
+        data.innerHTML = `
+        <div class="detail">
+            <h3 class="meaning">Word</h3>
+            <p>${result[0].word}</p>                
+        </div>
+
+        <div class="detail">
+            <h3 class="meaning">Definition</h3>
+            <p>${result[0].meanings[0].definitions[0].definition}</p>                
+        </div>
+
+        <div class="detail">
+            <h3 class="meaning">Example</h3>
+            <p>${result[0].meanings[0].definitions[0].example}</p>                
+        </div>
+
+        <div class="detail">
+            <h3 class="meaning">Part of Speech</h3>
+            <p>${result[0].meanings[0].partOfSpeech}</p>                
+        </div>
+
+        <div class="detail">
+            <h3 class="meaning">Origin</h3>
+            <p>${result[0].origin}</p>                
+        </div>
+        `;
+        console.log(result);
+    }).catch(() =>{
+        data.innerHTML = `<div class="error">Can't Find THE Meaning of <span>${text.value}</span>.Please Search Another Word.</div>
+        `;
+    });
+    
 }
+
+search.addEventListener('click', () => {
+    if (text.value != '') {
+        // run the function
+        loadData();
+    }else{
+        data.innerHTML = `<div class="impo">Type Any Word & Press Enter To Get Meaning.</div>`;
+    }
+});
